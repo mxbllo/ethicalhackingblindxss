bac2834281b196334fa41047ed62eb19da44089f
Max <159624837+mxbllo@users.noreply.github.com>
2024-05-17 04:19:52 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,16 @@
+var tab = document.getElementsByClassName("tab");
+var i;
+
+for (i = 0; i < tab.length; i++) {
+  tab[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+
